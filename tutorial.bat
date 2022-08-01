386e7b1531a1fd4abd57eff74280327bf64fc215
kgb <110420939+avoidingkgb@users.noreply.github.com>
2022-08-01 16:54:14 -0500

    Add files via upload

---
+++ tutorial.bat
@@ -0,0 +1,16 @@
+@echo off
+title discord.gg/comrades
+color 9
+echo Opening REGEDIT
+pause
+START regedit
+echo In top bar, put Computer\HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Class
+pause
+echo Now for your mac addy (RIGHT CLICK CONTROL AND FIND)
+pause
+getmac
+pause
+echo Search all the folders for your MAC | and change it
+pause 
+START https://discord.gg/comrades
+exit
